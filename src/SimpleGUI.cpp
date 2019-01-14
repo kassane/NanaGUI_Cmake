f6f200268ca5982884ebbc13839563e450bba606
Matheus Catarino <matheus-catarino@hotmail.com>
2019-01-14 16:51:23 -0300

    fix SimpleGUI

--- src/SimpleGUI.cpp
+++ src/SimpleGUI.cpp
@@ -1,6 +1,7 @@
 #include <nana/gui.hpp>
 #include <nana/gui/widgets/label.hpp>
 #include <nana/gui/widgets/button.hpp>
+#include <nana/gui/msgbox.hpp>
 #include <iostream>
 
 #if __has_include(<windows.h>)
@@ -15,17 +16,27 @@ class My_Widget : public form
     place place_    {*this};
     label lab_      {*this};
     button btn_     {*this};
+    msgbox msg_     {*this, "Simple - Warning", msgbox::button_t::yes_no};
     public:
         My_Widget() {
-            this->caption("Simple Notepad - Nana C++ Library");
+            this->caption("Simple GUI - Nana C++ Library");
             //Define a label and display a text.
             lab_.caption("Hello, <bold blue size=16>Nana C++ Library</>");
             lab_.format(true);
 
             //Define a button and answer the click event.
             btn_.caption("Quit");
+            msg_ << "Are you sure you want to close?";
             btn_.events().click([this]{
-            this->close();
+                switch (msg_.show())
+                {
+                case msgbox::pick_yes:
+                    this->close();
+                case msgbox::pick_no:
+                    break;
+                default:
+                    std::cerr << "[ERROR]: desconhecido!" << std::endl;
+                }
     });
             //Layout management
             place_.div("vert <><<><weight=80% text><>><><weight=24<><button><>><>");
